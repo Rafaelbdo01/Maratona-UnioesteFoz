44ef317d096a5da050e77b1fd6db65c2ad1822b2
Rafaelbdo01 <rbueno.oliveira7@gmail.com>
2026-09-14 12:47:08 -0300

    Minha quase-solução

---
+++ USACO/Simulacao/MeasuringTraffic.cpp
@@ -0,0 +1,104 @@
+//Link da questao: https://usaco.org/index.php?page=viewproblem2&cpid=917
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x .end()
+#define rall(x) x.rbegin(), x.rend()
+#define joaobobao cin.tie(0) -> sync_with_stdio(0)
+#define INF 1e18
+
+void solve(){
+
+    int n; cin >> n;
+    vector<tuple<string,int,int>> traffic;
+
+    for(int i=0;i<n;i++){
+
+        string s; int x, y;
+        cin >> s >> x >> y;
+        
+        traffic.pb({s, x, y});
+    }
+
+    pair<int,int>range = {0,1001};
+
+    //Antes de 1
+    for(int i=n-1;i>=0;i--){
+
+        auto [x,y,z] = traffic[i];
+
+        if(x == "on"){
+
+            range.first -= z;
+            range.second -= y;
+            continue;
+        }
+
+        else if(x == "off"){
+
+            range.first += y;
+            range.second += z;
+            continue;
+        }
+
+        else if(x == "none"){
+
+            range.first = max(range.first, y);
+            range.second = min(range.second, z);
+            continue;
+        }
+    }
+
+    cout << range.first << " " << range.second << endl;
+
+    range = {0, 1001};
+
+    //Apos N
+
+    for(int i=0;i<n;i++){
+
+        auto [x,y,z] = traffic[i];
+
+        if(x == "off"){
+
+            range.first -= z;
+            range.second -= y;
+            continue;
+        }
+
+        else if(x == "on"){
+
+            range.first += y;
+            range.second += z;
+            continue;
+        }
+
+        else if(x == "none"){
+
+            range.first = max(range.first, y);
+            range.second = min(range.second, z);
+            continue;
+        }
+    }
+
+    cout << range.first << " " << range.second << endl;
+
+}
+
+signed main(){
+
+    freopen("traffic.in", "r", stdin);
+    freopen("traffic.out", "w", stdout);
+    
+    joaobobao;
+
+    int t = 1;
+    //cin >> t;
+
+    while(t--) solve();
+
+    return 0;
+}
